5975a60f36cec1e2f45334351ea8e99528d94376
Pedro Marchese <rm563339@fiap.com.br>
2025-06-03 11:41:36 -0300

    fix: mouse enter, leave e mouse click

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ document.addEventListener("DOMContentLoaded", function () {
   const colorMenu = document.getElementById("colorMenu");
   const colorBox = document.getElementById("colorBox");
 
-  // Mostrar ou esconder colorBox ao passar o mouse
+  // Mostrar colorBox ao passar o mouse
   colorMenu.addEventListener("mouseenter", function () {
     colorBox.classList.remove("hidden");
   });
 
-  // Esconde o colorBox quando o mouse sai dele
+  // Esconder colorBox ao sair do mouse
   colorBox.addEventListener("mouseleave", function () {
     colorBox.classList.add("hidden");
+  });
+
+  // Alternar colorBox ao clicar
+  colorMenu.addEventListener("click", function () {
+    colorBox.classList.toggle("hidden");
   });
 
   // Altera o fundo com getElementById
@@ -24,4 +29,8 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("color3").addEventListener("click", function () {
     document.body.style.backgroundColor = getComputedStyle(this).backgroundColor;
   });
+});
+const navLinks = document.querySelectorAll("nav a, nav button, nav li");
+navLinks.forEach(function(link) {
+  link.style.cursor = "pointer";
 });
